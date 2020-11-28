a0fafb872b96b22ed99341848deec28697240e9e
TTOzzi <devttozzi@gmail.com>
2020-11-28 14:56:29 +0900

    feat: SideDishHader 구현
    리스트에서 헤더를 나타낼 뷰 구현

---
+++ SideDish/SideDish/SideDishHeader.swift
@@ -0,0 +1,41 @@
+//
+//  SideDishHeader.swift
+//  SideDish
+//
+//  Created by TTOzzi on 2020/11/28.
+//
+
+import SwiftUI
+
+struct SideDishHeader: View {
+    let title: String
+    let category: String
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            VStack {
+                Badge(title: category,
+                      textColor: .secondary,
+                      backgroundColor: .white)
+                    .border(Color.secondary, width: 1)
+                
+                Text(title)
+                    .font(.system(size: 20, weight: .bold))
+            }
+            
+            Spacer()
+        }
+        .padding(.bottom)
+        .background(Color.white)
+    }
+}
+
+struct SideDishHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        SideDishHeader(title: "언제 먹어도 든든한 밑반찬",
+                       category: "밑반찬")
+            .previewLayout(.fixed(width: 350, height: 80))
+    }
+}
