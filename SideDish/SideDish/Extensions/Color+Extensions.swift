2fc295ce071e798a9b1eab89ef0c5f8f7d97b839
TTOzzi <devttozzi@gmail.com>
2020-11-28 13:45:12 +0900

    feat: Color extension 추가
    Asset의 컬러를 불러오는데 사용할 extension

---
+++ SideDish/SideDish/Extensions/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  SideDish
+//
+//  Created by TTOzzi on 2020/11/28.
+//
+
+import SwiftUI
+
+extension Color {
+    static var keyColor: Color {
+        Color("keyColor")
+    }
+}
