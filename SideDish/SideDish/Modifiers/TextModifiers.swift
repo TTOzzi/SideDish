cdfc13172c31380b29e6f6219fe186e8e2c7edf0
TTOzzi <devttozzi@gmail.com>
2020-11-28 13:47:40 +0900

    feat: Subtitle CustomModifier 구현
    중복되어 사용되는 text 설정 코드를 CustomModifier를 만들어 분리

---
+++ SideDish/SideDish/Modifiers/TextModifiers.swift
@@ -0,0 +1,22 @@
+//
+//  TextModifiers.swift
+//  SideDish
+//
+//  Created by TTOzzi on 2020/11/28.
+//
+
+import SwiftUI
+
+struct Subtitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.secondary)
+            .font(.system(size: 14))
+    }
+}
+
+extension Text {
+    func subtitle() -> some View {
+        modifier(Subtitle())
+    }
+}
