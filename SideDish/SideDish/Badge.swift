8d6221b875a2d904c9a888d7bf195b7a543e0534
TTOzzi <devttozzi@gmail.com>
2020-11-28 13:45:51 +0900

    feat: Badge 뷰 인자 추가
    글자의 색깔도 외부에서 받아오도록 수정

--- SideDish/SideDish/Badge.swift
+++ SideDish/SideDish/Badge.swift
@@ -9,21 +9,22 @@ import SwiftUI
 
 struct Badge: View {
     let title: String
-    let color: Color
+    let textColor: Color
+    let backgroundColor: Color
     
     var body: some View {
         Text(title)
-            .foregroundColor(.white)
+            .foregroundColor(textColor)
             .font(.system(size: 12))
             .padding(.vertical, 2)
             .padding(.horizontal, 4)
-            .background(color)
+            .background(backgroundColor)
     }
 }
 
 struct Badge_Previews: PreviewProvider {
     static var previews: some View {
-        Badge(title: "론칭특가", color: .purple)
+        Badge(title: "론칭 특가", textColor: .white, backgroundColor: .purple)
             .previewLayout(.fixed(width: 100, height: 100))
     }
 }
